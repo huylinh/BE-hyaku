9974314306a8c762fd61cb29146f0cbab0b1df9a
Vu-nh1109 <vungohoang2001@gmail.com>
2023-06-03 17:57:03 +0700

    add controller and routes for Store's CRUD funtions

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StoreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/stores', [StoreController::class, 'index']);
+Route::get('/stores/{id}', [StoreController::class, 'show']);
+Route::post('/stores', [StoreController::class, 'store']);
+Route::put('/stores/{id}', [StoreController::class, 'update']);
+Route::delete('/stores/{id}', [StoreController::class, 'destroy']);
