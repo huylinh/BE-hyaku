5b1d3e8fd032ef436794a46e80cd7cb78d03f912
Vu-nh1109 <vungohoang2001@gmail.com>
2023-06-20 19:08:41 +0700

    temporary HasMany relationship between Review and History

--- app/Models/History.php
+++ app/Models/History.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class History extends Model
@@ -21,8 +22,13 @@ class History extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
-    public function review(): HasOne
+    // public function review(): HasOne
+    // {
+    //     return $this->hasOne(Review::class, 'history_id');
+    // }
+
+    public function review(): HasMany
     {
-        return $this->hasOne(Review::class, 'history_id');
+        return $this->hasMany(Review::class, 'history_id');
     }
 }
